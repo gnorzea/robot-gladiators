d7e5c7231b3a9fc6860e2169452f3a4b40c2d90a
Thunnder <gnorzea@gmail.com>
2022-03-30 00:57:31 -0400

    randomized fight order

--- assets/game.js
+++ assets/game.js
@@ -26,12 +26,18 @@ var fightOrSkip = function () {
 }
 
 var fight = function (enemy) {
+  // keep track of who goes first
+  var isPlayerTurn = true;
+  if (Math.random() > 0.5) {
+    isPlayerTurn = false;
+  }
   // repeat and execute as long as the enemy-robot is alive 
   while (playerInfo.health > 0 && enemy.health > 0) {
-    if (fightOrSkip()) {
-      break;
-    }
-
+    if (isPlayerTurn) {
+      if (fightOrSkip()) {
+        break;
+      }
+    
     // remove enemy's health by subtracting the amount set in the playerInfo.attack variable
     var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
 
@@ -52,12 +58,22 @@ var fight = function (enemy) {
     } else {
       window.alert(enemy.name + " still has " + enemy.health + " health left.");
     }
+  }  else {
+    var damage = randomNumber(enemy.attack - 3, enemy.attack);
+  }
 
     // remove player's health by subtracting the amount set in the enemy.attack variable
-    playerInfo.health = playerInfo.health - enemy.attack;
-    console.log(
-      enemy.name + " attacked " + playerInfo.name + ". " + playerInfo.name + " now has " + playerInfo.health + " health remaining."
-    );
+    playerInfo.health = Math.max(0, playerInfo.health - damage);
+      console.log(
+        enemy.name +
+          " attacked " +
+          playerInfo.name +
+          ". " +
+          playerInfo.name +
+          " now has " +
+          playerInfo.health +
+          " health remaining."
+      );
 
     // check player's health
     if (playerInfo.health <= 0) {
@@ -66,7 +82,9 @@ var fight = function (enemy) {
     } else {
       window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
     }
+    isPlayerTurn = !isPlayerTurn
   }
+  
 };
 var startGame = function () {
   playerInfo.health = 100;
@@ -116,8 +134,8 @@ var endGame = function () {
 };
 var shop = function () {
   var shopOptionPrompt = window.prompt(
-    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."    );
-    shopOptionPrompt = parseInt(shopOptionPrompt);
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE.");
+  shopOptionPrompt = parseInt(shopOptionPrompt);
   // use switch to carry out action
   switch (shopOptionPrompt) {
     case 1:
